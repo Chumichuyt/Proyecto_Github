25523cb3a3f8ac2ba5d98b113b95b96fc8bb7b9d
Alvariill0 <alvarillo0412@gmail.com>
2024-05-30 13:30:27 +0200

    Actualizada la forma en la que se importan los módulos

--- centro1.py
+++ centro1.py
@@ -1,5 +1,7 @@
-import cancha as c
-import reservas as r
+from cancha import Cancha
+from reservas import Reserva
+#from persona import Persona
+
 class Centro:
     def __init__(self, nombre, direccion):
         self.nombre = nombre
@@ -7,14 +9,3 @@ class Centro:
         self.lista_canchas = []
         self.lista_clientes = []
 
-centro = Centro("Centro 1", "Calle 1")
-
-cancha1 = c.Cancha()
-cancha1.crear_cancha()
-
-centro.lista_canchas.append(cancha1)
-
-deportes_disponibles = []
-
-cancha1.mostrar_deportes_disponibles(centro, deportes_disponibles)
-cancha1.mostrar_canchas_para_deporte("Futbol", centro)
